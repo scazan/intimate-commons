e273a0941724b62e5b3a8ce28937282f1e4a473b
Scott Cazan <scottcazan@gmail.com>
2023-08-19 20:03:25 +0200

    add border just for kicks

--- src/components/Choice/index.tsx
+++ src/components/Choice/index.tsx
@@ -4,7 +4,7 @@ export const Choice = (props: { label?: string; value: string }) => {
   const { label = props.value, value } = props;
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center space-x-2 border p-2">
       <RadioGroupItem value={value} id={value} />
       <Label htmlFor={value}>{label}</Label>
     </div>
